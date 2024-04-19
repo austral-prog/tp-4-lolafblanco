6f90de13740ee74de95d238a79bdf47c796f254f
lolafblanco <162721872+lolafblanco@users.noreply.github.com>
2024-04-19 09:08:22 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,11 +1,12 @@
 def leap_year():
 
-    año = int(input("Ingrese un año: "))
+    years = int(input("Ingrese un año: "))
     if years%400 == 0:
-        print("El año"+" "+ str(years)+ "es bisiesto")
+        print("El año"+" "+ str(years)+ " es bisiesto")
     elif years%4==0 and years%100!=0:
-        print("El año"+" "+str(years)+ "es bisiesto")
+        print("El año"+" "+str(years)+ " es bisiesto")
     else:
         print("El año"+" "+str(years)+ " no es bisiesto")
+
 
     
