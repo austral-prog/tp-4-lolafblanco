364b04316c0b8f3b1c832649bc19c091c76df29e
lolafblanco <162721872+lolafblanco@users.noreply.github.com>
2024-04-19 07:26:18 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,21 +1,28 @@
+import math
 def line():
-    import math
-    a=float(input("Ingrese el coeficiente A:"))
-    b=float(input("Ingrese el coeficiente B:"))
-    X1=float(input("Ingrese el coeficiente X1:"))
-    X2=float(input("Ingrese el coeficiente X2:"))
+    
+    a= float (input("Ingrese el coeficiente A:"))
+    b= float (input("Ingrese el coeficiente B"))
+    X1= float (input("Ingrese el coeficiente X1"))
+    X2= float (input("Ingrese el coeficientte X2"))
+    Y1=float(a*x1+b)
+    Y2=float(a*x2+b)
+    P1= [X1, Y1]
+    P2= [X2, Y2]
 
-    y1= a*x1+b
-    y2= a*x2+b
-    print(f"""El coeficiente A de su ecuacion de la recta es: {a}
-El coeficiente B de su ecuacion de la recta es: {b}
-El coeficiente X1 de su ecuacion de la recta es: {x1}
-El coeficiente X2 de su ecuacion de la recta es: {x2}""")
-    print(f"""\nPara la siguiente ecuacion:
-    Y = {a}X + {b}""")
-    print(f"""\nDados los siguientes puntos:
-    P1 ({x1 , {y1})
-    P2 ({x2 , {y2})""")
+    print(f"""El coeficiene A de su ecuación de la recta es: {a}
+        El coeficiente B de su ecuación de la recta es: {b}
+        El coeficiente X1 de su ecuación de la recta es: {x1}
+        El coeficiente X2 de su ecuación de la recta es: {x2}""")
+    print()
+    print("Para la siguiente ecuación:")
+print(f"tY = {a}X + {b}")
+print()
+print("Dados los siguientes puntos:")
+print(f"\tP1 ({X1}, {Y1})")
+print(f"\tP2 ({X2}, {Y2})")
+print()
+print("La distancia entre ellos es:" + str(math.dist(P1,P2)))
 
     d= math.sqrt((x2-x1)**2 + (y2-y1)**2
     print(f"\nLa distancia entre ellos es: {d}")
